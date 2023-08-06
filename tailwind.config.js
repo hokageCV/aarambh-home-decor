d577f96b1d95022fbee6922ed29b59cc18c5a8df
hokageCV <chaitanyavaru@gmail.com>
2023-08-06 17:48:57 +0530

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        mono: ["Consolas", "Cascadia Code", "Menlo", "ui-monospace", "monospace"],
       },
+      colors: {
+        baseBG: "var(--baseBG)",
+        primaryText: "var(--primaryText)",
+        secondaryText: "var(--secondaryText)",
+        cardBG: "var(--cardBG)",
+        cardBorder: "var(--cardBorder)",
+        btnBG: "var(--btnBG)",
+        btnText: "var(--btnText)",
+      }
     },
   },
   plugins: [],
